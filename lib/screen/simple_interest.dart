bd2658d28a6f533bbaa988df9ff75c23edc5b4a2
Sandeep <sandeep@Sandeeps-MacBook-Air.local>
2024-04-18 01:54:56 +0545

    changed buttons and text alignments

--- lib/screen/simple_interest.dart
+++ lib/screen/simple_interest.dart
@@ -18,7 +18,7 @@ class _SimpleInterestScreenState extends State<SimpleInterestScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'Simple Interest',
+          'Simple Interest Calculator',
           style: TextStyle(
             fontSize: 30,
           ),
@@ -26,13 +26,14 @@ class _SimpleInterestScreenState extends State<SimpleInterestScreen> {
         elevation: 0,
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const Text(
             'Principle',
             style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.italic),
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+            ),
           ),
           TextField(
             keyboardType: TextInputType.number,
@@ -52,9 +53,9 @@ class _SimpleInterestScreenState extends State<SimpleInterestScreen> {
           const Text(
             'Rate',
             style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.italic),
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+            ),
           ),
           TextField(
             keyboardType: TextInputType.number,
@@ -74,9 +75,9 @@ class _SimpleInterestScreenState extends State<SimpleInterestScreen> {
           const Text(
             'Time',
             style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.italic),
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+            ),
           ),
           TextField(
             keyboardType: TextInputType.number,
@@ -89,25 +90,29 @@ class _SimpleInterestScreenState extends State<SimpleInterestScreen> {
                 border: OutlineInputBorder(), hintText: 'Enter your time'),
           ),
           const SizedBox(
-            height: 8,
+            height: 30,
           ),
-          ElevatedButton(
-            onPressed: () {
-              setState(() {
-                if (principle != null && rate != null && time != null) {
-                  result = (principle! * rate! * time! ~/ 100);
-                }
-              });
-            },
-            child: const Text('Calculate'),
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  if (principle != null && rate != null && time != null) {
+                    result = (principle! * rate! * time! ~/ 100);
+                  }
+                });
+              },
+              child: const Text('Calculate'),
+            ),
           ),
           const SizedBox(
-            height: 18,
+            height: 20,
           ),
-          Text(
-            'Your Simple Interest is $result',
-            style: const TextStyle(
-              fontSize: 30,
+          Center(
+            child: Text(
+              'Your Simple Interest is $result',
+              style: const TextStyle(
+                fontSize: 25,
+              ),
             ),
           )
         ],
